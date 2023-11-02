6f5448cc175d18dfc6030bb87f09168119fcaa29
Aarif Khan <ajajchauhan@gmail.com>
2023-11-02 08:46:14 +0530

    Update appSettings for timestamp in str

--- appSettings.py
+++ appSettings.py
@@ -6,10 +6,11 @@ useConf = 0.4
 MONGODB_URI = "mongodb://172.22.129.175:12345/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.0.2"
 MODEL_NAME = "yolov8n.pt"
 
+import time
 import datetime
 
 def getTimestring():
     return datetime.datetime.now().strftime("%A, %d %B %Y %I:%M:%S%p")
 
 def getTimestamp():
-    return datetime.datetime.now()
+    return str(time.time())
