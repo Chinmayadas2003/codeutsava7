a1a2696a99dfc10a14b9c132015ef592de3f7844
Aarif Khan <ajajchauhan@gmail.com>
2023-11-02 09:33:42 +0530

    endpoint for sending filled potholes

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ def handle_error(e):
     print(e)
     return "Error!"
 
-# GeoJSON Imports
+# GeoJSON and GeoPY Imports
 from geojson import Point
+from geopy.distance import geodesic as GD
 
 # MongoDB Imports
 from pymongo.mongo_client import MongoClient
@@ -189,7 +190,7 @@ def save_pothole_from_live():
         except Exception as e:
             return handle_error(e)
 
-# Handle File Upload and Download from Android App
+# Handle File Upload and Download from Android App with Inference
 
 @app.route('/uploads/<path:filename>')
 def download_file(filename):
@@ -280,6 +281,99 @@ def upload_file():
       <input type=submit value=Upload>
     </form>
     '''
+
+# Pothole elimination from database using Haversine formula
+@app.route("/eliminate", methods=["POST"])
+def eliminate_potholes():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        uploads = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'])
+        # This contains the data sent from frontend
+        try:
+            lat = request.form['lat']
+            lon = request.form['lon']
+            print("Latitude:", lat)
+            print("Longitude:", lon)
+        except Exception as e:
+            return handle_error(e)
+        
+        # Run Inference and save output
+        try:
+            # file.save(os.path.join(uploads, filename))
+            filestr = request.files['file'].read()
+            # file_bytes = np.fromstring(filestr, np.uint8)
+            nparr = np.fromstring(filestr, np.uint8)
+            img = cv2.imdecode(nparr, cv2.IMREAD_ANYCOLOR)
+            results = model(img, stream=True)
+            boxCount = 0
+            for r in results:
+                boxes = r.boxes
+                for box in boxes:
+                    # Bounding Box
+                    x1, y1, x2, y2 = box.xyxy[0]
+                    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
+                    # cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3)
+                    w, h = x2 - x1, y2 - y1
+                    # cvzone.cornerRect(img, (x1, y1, w, h))
+
+                    # Confidence
+                    conf = math.ceil((box.conf[0] * 100)) / 100
+
+                    # Class Name
+                    cls = int(box.cls[0])
+                    currentClass = classNames[cls]
+                    # print(currentClass)
+
+                    if conf > useConf:
+                        boxCount += 1
+                        # cvzone.putTextRect(img, f'{classNames[cls]} {conf}',
+                                        #   (max(0, x1), max(35, y1-10)), scale=2, thickness=2,colorB=myColor,
+                                        #   colorT=(255,255,255),colorR=myColor, offset=5)
+                        cv2.rectangle(img, (x1, y1), (x2, y2), myColor, 2)
+                        # currentArray = np.array([x1, y1, x2, y2, conf])
+                        # detections = np.vstack((detections, currentArray))
+            if boxCount == 0:
+                # Fetching latitudes and longitudes from DB, 
+                # Removing using Haversine Formula
+                my = (lat, lon)
+                client.admin.command('ping')
+                readCursor = db.potholes.find({})
+                objListRem = []
+                for doc in readCursor:
+                    curr = (doc['lat'], doc['lon'])
+                    if GD(my,curr).km < 0.01:
+                        objListRem.append(doc['_id'])
+                print("Potholes to set to repaired: ", objListRem)
+                db.potholes.update_many(
+                    {
+                        "_id": {"$in": objListRem}
+                    },
+                    {
+                        "$set": {
+                            "repaired": True
+                        }
+                    }
+                )
+                return {"status":"true"}
+            else:
+                currTS = getTimestamp()
+                print("Pothole Detected")
+                cv2.imwrite(app.root_path + "/static_detect/wrong" + currTS + ".jpg", img)
+                print("File Saved:", app.root_path + "/static_detect/wrong" + currTS + ".jpg")
+                return {"status":"false"}
+        except Exception as e:
+            return handle_error(e)
 
 # Function call to make network requests
 def writeData(lat, lon, imgName, area):
--- appSettings.py
+++ appSettings.py
@@ -1,10 +1,10 @@
 classNames = ['Pothole']
 myColor = (0, 0, 255)
 my_map = {3: 1}
-useCuda = True
+useCuda = False
 useConf = 0.4
 MONGODB_URI = "mongodb://172.22.129.175:12345/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.0.2"
-MODEL_NAME = "yolov8n.pt"
+MODEL_NAME = "potholev1.pt"
 
 import time
 import datetime
@@ -13,4 +13,4 @@ def getTimestring():
     return datetime.datetime.now().strftime("%A, %d %B %Y %I:%M:%S%p")
 
 def getTimestamp():
-    return str(time.time())
+    return str(time.time()).replace('.', '', 1)
